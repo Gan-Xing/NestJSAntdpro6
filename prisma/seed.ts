4e8fd89e2d402badbfcf7ff28e98c4bb576c448f
Gan-Xing <599153574@qq.com>
2023-09-29 20:15:48 +0800

    创建假数据前，先清空数据库，创建用户为密码加密

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 import { fakerZH_CN as faker } from '@faker-js/faker';
+import { hash } from 'bcrypt';
 
 const prisma = new PrismaClient();
 
+async function deleteAllData() {
+  await prisma.article.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.permission.deleteMany();
+  await prisma.role.deleteMany();
+  await prisma.permissionGroup.deleteMany();
+}
+
 async function main() {
+  await deleteAllData();
   await createRoles();
   await createPermissionGroups();
   await createPermissions();
@@ -60,11 +70,47 @@ async function createPermissions() {
 async function createUsers() {
   const roles = await prisma.role.findMany();
 
+  const hashedAdminPassword = await hash('admin123', 10);
+
+  const existingAdmin = await prisma.user.findUnique({
+    where: { email: 'admin@example.com' },
+  });
+
+  if (existingAdmin) {
+    await prisma.user.update({
+      where: { email: 'admin@example.com' },
+      data: {
+        password: hashedAdminPassword,
+        roles: {
+          connect: roles.map((role) => ({ id: role.id })),
+        },
+      },
+    });
+  } else {
+    await prisma.user.create({
+      data: {
+        email: 'admin@example.com',
+        password: hashedAdminPassword,
+        username: 'admin',
+        gender: 'Male',
+        departmentId: 1,
+        isAdmin: true,
+        avatar: 'https://gravatar.com/avatar/0000?d=mp&f=y',
+        roles: {
+          connect: roles.map((role) => ({ id: role.id })),
+        },
+      },
+    });
+  }
+
   for (let i = 0; i < 100; i++) {
+    const plainPassword = faker.internet.password();
+    const hashedPassword = await hash(plainPassword, 10);
+
     await prisma.user.create({
       data: {
         email: faker.internet.email(),
-        password: faker.internet.password(),
+        password: hashedPassword,
         username: faker.internet.userName(),
         gender: faker.helpers.arrayElement(['Male', 'Female', 'Other']),
         departmentId: Math.floor(Math.random() * 10) + 1,
